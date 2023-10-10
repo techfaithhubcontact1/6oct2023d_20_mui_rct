31ac362ec48362492dbe41091cab7aa8d37871a4
“techfaithhubcontact1” <gulatiyajamte@gmail.com>
2023-10-10 19:48:25 +0530

    c2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home'
+import Contact from './Pages/Contact'
+import About from './Pages/About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={ <Home/> }></Route>
+        <Route path="/home" element={ <Home/> }></Route>
+        <Route path="/contact" element={ <Contact/> }></Route>
+        <Route path="/about" element={ <About/> }></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+//1. Import Area
+import { Box, Typography, styled } from '@mui/material'
+import React from 'react'
+
+//2. defination and export Area
+// const Wrapper = styled('div');
+// const Heading = styled( 'header');
+
+
+export default function Home() {
+  return (
+    <>
+      <Box>Box is mui component</Box>
+      <Typography> Typography is mui component </Typography>
+    </>
+  )
+}
